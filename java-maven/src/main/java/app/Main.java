ef8b5a3082211f84ca6e6bd4a6e6f1f2fdcc9129
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-02-27 19:52:23 -0600

    Added env PORT

--- java-maven/src/main/java/app/Main.java
+++ java-maven/src/main/java/app/Main.java
@@ -3,6 +3,8 @@ package app;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,12 +17,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Dotenv dotenv = Dotenv.configure().filename(".env").ignoreIfMissing().load();
+        int port = Optional.ofNullable(dotenv.get("PORT"))
+                .map(Integer::parseInt)
+                .orElse(3000);
 
-        var port = 3000;
         var server = HttpServer.create(new InetSocketAddress(port), 0);
 
         server.createContext("/", exchange -> {
-            var response = "Hello " + dotenv.get("MESSAGE");
+            var response = "Hello World!";
 
             exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=UTF-8");
             exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
