faded7132fb25a11a74e18df4fd602c9f81e5f70
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-03-04 16:41:38 -0600

    updated rust

--- rust/tests/lib_test.rs
+++ rust/tests/lib_test.rs
@@ -3,9 +3,11 @@ use hello_world_rust::greet;
 #[test]
 fn test_greet_world() {
     assert_eq!(greet("world"), "Hello, world!");
+    println!("t1")
 }
 
 #[test]
 fn test_greet_custom_name() {
     assert_eq!(greet("Rust"), "Hello, Rust!");
+    println!("t2")
 }
