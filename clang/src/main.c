82b995bb9fa4d6920e72592486aefa82d1e5b714
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-03-04 23:03:43 -0600

    Added clang

---
+++ clang/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "util/fun.h"
+
+int main() {
+  char* str = "Hola Mundo";
+  printf("%s\n", str);
+  hola();
+  return 0;
+}
---
+++ clang/src/util/fun.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void hola() {
+  printf("Hola Mundo!\n");
+}
