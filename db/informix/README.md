6c2dd9c60d23406daef9eef73d85ad872b9cb617
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-08-14 00:05:16 -0600

    updated

--- db/informix/README.md
+++ db/informix/README.md
@@ -1,6 +1,15 @@
+```sh
 Host: localhost
 Port: 9088
 Server: informix
-Database: sysmaster
+Database: db
 User: informix
 Password: in4mix
+```
+
+```sh
+# permission
+chmod -R 777 ./informix-init
+# run files
+./dbaccess src/run.sql
+```
---
+++ db/informix/src/run.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    username    VARCHAR(50)  NOT NULL UNIQUE,
+    email       VARCHAR(100) NOT NULL,
+    password    VARCHAR(255) NOT NULL,
+    created_at  DATETIME YEAR TO SECOND DEFAULT CURRENT YEAR TO SECOND
+);
