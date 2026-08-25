d4f32c996e6e4c2081c8e289440fb2c2704ea23f
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-08-25 03:25:51 -0600

    updated

--- web/bun/test/env.test.js
+++ web/bun/test/env.test.js
@@ -2,5 +2,9 @@ import { test } from 'bun:test'
 import { env } from 'utils'
 
 test('env', () => {
-  console.info(env)
+  console.info(env.PUBLIC_VAR)
+})
+
+test('hi', () => {
+  console.info('hola')
 })
